765a78dbe038f648b144e2b7bf4b87021db281a0
hidenorly <twitte.harold@gmail.com>
2024-06-16 12:35:03 +0900

    Add -n option on filecount2.py

--- filecount2csv.py
+++ filecount2csv.py
@@ -15,7 +15,6 @@
 import os
 import re
 import argparse
-import csv
 
 def count_files(directory, file_pattern):
     file_count = 0
@@ -25,29 +24,38 @@ def count_files(directory, file_pattern):
                 file_count += 1
     return file_count
 
-def generate_report(directory, output_file, file_pattern):
+def generate_report(directory, output_file, file_pattern, dont_output_if_zero = False):
     report = []
     for root, dirs, files in os.walk(directory):
         folder_name = os.path.relpath(root, directory)
         file_count = count_files(root, file_pattern)
         report.append((folder_name, file_count))
 
+    file_writer = None
     if output_file:
-        with open(output_file, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerows(report)
-    else:
-        for folder_name, file_count in report:
-            print(f"{folder_name},{file_count}")
+        file_writer = open(output_file, 'w', newline='')
+
+    for folder_name, file_count in report:
+        line = f"{folder_name},{file_count}"
+        if not dont_output_if_zero or file_count:
+            if file_writer:
+                file_writer.write(line+"\n")
+            else:
+                print(line)
+
+    if file_writer:
+        file_writer.close()
+        file_writer = None
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", dest="target_directory", default=".", help="Target Directory")
     parser.add_argument("-o", dest="output_file", help="CSV file to output the results")
     parser.add_argument("-f", dest="file_pattern", default=None, help="Regular expression pattern for file matching")
+    parser.add_argument("-n", dest="no_zero", default=False, action="store_true", help="Set if excludes 0 file count directory")
     args = parser.parse_args()
 
-    generate_report(args.target_directory, args.output_file, args.file_pattern)
+    generate_report(args.target_directory, args.output_file, args.file_pattern, args.no_zero)
 
 
 
